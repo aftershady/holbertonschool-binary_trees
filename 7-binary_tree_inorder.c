ed3e6f6144c68d0e76a8f5738b03651ec328ba99
Jonathan <shady14@hotmail.fr>
2024-08-06 09:50:58 +0200

    remove condition to see if the node is the parent

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,25 +1,7 @@
 #include "binary_trees.h"
-#include <stdio.h>
 
 /**
- * _print_tree - print all nodes in order traversal
- * @tree: pointer on the first node
- * @func: function pointer on the function print_node
- * Description: use recursion to start to printf from the last node
- * on the left to the last node on the right
- * Return: nothing
- */
-void _print_tree(const binary_tree_t *tree, void (*func)(int))
-{
-	if (!tree || !func)
-	return;
-
-	_print_tree(tree->left, func);
-	func(tree->n);
-	_print_tree(tree->right, func);
-}
-/**
- * binary_tree_inorder - check if node is right before print
+ * binary_tree_inorder - print all nodes in order traversal
  * @tree: pointer to a node in the binary tree
  * @func: function pointer on the function print_node
  * Description: check if the node given is not empty and if it's
@@ -32,10 +14,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (!tree || !func)
 		return;
 
-	while (tree->parent)
-	{
-		tree = tree->parent;
-	}
-
-	_print_tree(tree, func);
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
