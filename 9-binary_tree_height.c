567b7b8e16efeb6a632a422332f814fc1ab49ca0
Jonathan <shady14@hotmail.fr>
2024-08-06 11:09:25 +0200

    forget to delete the printf test notes

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,6 +1,4 @@
 #include "binary_trees.h"
-#include <stdio.h>
-
 /**
  * binary_tree_height - give the height of a node of a binary tree
  * @tree: pointer to the node of the binary tree
@@ -14,11 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	left_height = binary_tree_height(tree->left);
-	printf("left value %lu\n", left_height);
 	right_height = binary_tree_height(tree->right);
-	printf("left value %lu\n", left_height);
-	printf("right value %lu\n", right_height);
-
 
 	return ((left_height > right_height ? left_height : right_height) + 1);
 }
