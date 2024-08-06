b8239fbd51a50a2a4764023d2103ac7dedb6d741
DerfM53 <frederic.mieuzet@gmail.com>
2024-08-06 18:23:52 +0200

    create ann complete file for task 13 project 0

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (!tree)
+	{
+		return (0);
+	}
+	if (tree->left || tree->right)
+	{
+		count++;
+	}
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
+
+	return (count);
+}
