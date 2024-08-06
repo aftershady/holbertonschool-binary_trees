67826ff190d95913f830f22fce1af3e18d2ebbeb
Jonathan <shady14@hotmail.fr>
2024-08-06 09:45:19 +0200

    remove loop to move the node to the root

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,34 +1,17 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 /**
- * delete_node - delete the binary tree
- * @tree: first node of binary tree
- * Return : nothing
- */
-void delete_node(binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return;
-
-	delete_node(tree->left);
-	delete_node(tree->right);
-	free(tree);
-}
-/**
  * binary_tree_delete - use recursion to delete a binary three
  * @tree: tree send
  * Return: nothing
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-
 	if (!tree)
 	return;
 
-	while (tree->parent != NULL)
-	{
-		tree = tree->parent;
-	}
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 
-	delete_node(tree);
 }
--- 3-main.c
+++ 3-main.c
@@ -17,7 +17,6 @@ int main(void)
     binary_tree_insert_right(root->left, 54);
     binary_tree_insert_right(root, 128);
     binary_tree_print(root);
-	root = root->left;
     binary_tree_delete(root);
     return (0);
 }
