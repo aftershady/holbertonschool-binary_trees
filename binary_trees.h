5e2d84b24dd69ea014afd622f67a7b113a8ea89d
DerfM53 <frederic.mieuzet@gmail.com>
2024-08-07 08:35:36 +0000

    create and complete file for task 15

--- binary_trees.h
+++ binary_trees.h
@@ -1,4 +1,3 @@
-
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 #include <stddef.h>
@@ -12,10 +11,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -39,7 +38,7 @@ size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
 
 
 #endif
-
