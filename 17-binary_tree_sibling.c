92a9a304012007a03cda7e263bd0d00412c44d00
DerfM53 <frederic.mieuzet@gmail.com>
2024-08-08 08:28:36 +0000

    correction file 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,12 +10,12 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	binary_tree_t *parent = node->parent;
+
 	if (!node)
 	{
 		return (NULL);
 	}
-
-	binary_tree_t *parent = node->parent;
 
 	if (!parent)
 	{
